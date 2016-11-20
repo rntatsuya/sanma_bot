2d43c194f82680d5fc8dc14f53ab3bf0dcba3322
Tatsuya Yokota <tyokota@colby.edu>
2016-11-20 11:36:01 -0500

    initial commit

---
+++ twitterBot.js
@@ -0,0 +1,42 @@
+/*
+	Tatsuya Yokota
+	11/20/2016
+	
+	Aim: Every 10 min, find 5 most recent tweets with the keyword サンマ and
+		retweet it.
+	
+	1. Find 5 most recent tweets with keyword
+	2. Retweet automatically
+	3. Execute function every 10 min
+	
+*/
+
+var Twit = require('twit');
+
+var T = new Twit({
+    consumer_key:         '' // Your Consumer Key
+  , consumer_secret:      '' // Your Consumer Secret
+  , access_token:         '' // Your Access Token
+  , access_token_secret:  '' // Your Access Token Secret
+});
+
+
+findAndRetweetSanmaTweets = function() {
+	T.get('search/tweets', { q: 'サンマ', result_type: 'recent', lang: 'ja', count: 5 }, function(err1, data1, response1) {
+	  // data1 is an object with an array called statuses
+	  // statuses has objects that have info of the tweet/account id/ etc as keys and values
+	  for (var i = 0; i < data1.statuses.length; i++) {
+		T.post('statuses/retweet/:id', { id: data1.statuses[i].id_str }, function (err, data, response) {
+		});
+	  }
+	});
+}
+
+setInterval(function() {
+  try {
+    findAndRetweetSanmaTweets();
+  }
+  catch (e) {
+    console.log(e);
+  }
+}, 60000 * 10); // 60000 millisecond = 1 minute
